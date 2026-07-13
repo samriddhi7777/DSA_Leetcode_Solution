691352e5e25b5122cc0ee3b7c081c9c1c52cce53
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-13 23:19:29 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/Permutation in String/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution{
+    public:
+    bool checkInclusion(string s1, string s2){
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26,0);
+
+        for(char ch : s1){
+            freq1[ch - 'a']++;
+        }
+        int left = 0;
+        for(int right = 0;right < s2.size(); right++){
+            freq2[s2[right] - 'a']++;
+            if(right - left + 1 > s1.size()){
+                freq2[s2[left] - 'a']--;
+                left++;
+
+
+            }
+            if(freq1 == freq2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+        
